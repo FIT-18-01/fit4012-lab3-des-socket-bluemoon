b0b982b370786084c5da1747770aa499c77aa5e4
trung-2006 <tranloi1982tb@gmail.com>
2026-05-08 11:34:43 +0700

    Trung: Fix requirements file extension to pass CI

--- tests/test_tamper_negative.py
+++ tests/test_tamper_negative.py
@@ -1,4 +1,4 @@
-from des_socket_utils import encrypt_des_cbc, decrypt_des_cbc
+sfrom des_socket_utils import encrypt_des_cbc, decrypt_des_cbc
 
 
 def test_tampered_ciphertext_should_fail_or_change_plaintext():
